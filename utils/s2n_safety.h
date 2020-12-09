58fb2b5c4b26684b56478f692b71f16a311e3777
Steven Collison <steven@raycoll.com>
2020-12-08 20:19:39 -0800

    Use pthread_once to initialize per process state
    The error handling feedback from pthread_once is wonky. We're relying
    on s2n_errno being set properly after the function is executed.

--- utils/s2n_safety.h
+++ utils/s2n_safety.h
@@ -233,6 +233,11 @@
 #define GUARD_RESULT_NONNULL( x )                   __S2N_ENSURE((x) != NULL, return S2N_RESULT_ERROR)
 
 /**
+ * Ensures `x` is ok, otherwise the function will return
+ */
+#define GUARD_RESULT_VOID( x )                   __S2N_ENSURE(s2n_result_is_ok(x), return)
+
+/**
  * Ensures `x` is not a OpenSSL error, otherwise the function will `BAIL` with `error`
  */
 /* TODO: use the OSSL error code in error reporting https://github.com/awslabs/s2n/issues/705 */
@@ -262,6 +267,12 @@
  * Ensures `x` is not `NULL`, otherwise the function will return a POSIX error (`-1`)
  */
 #define GUARD_POSIX_NONNULL( x )                    __S2N_ENSURE((x) != NULL, return S2N_FAILURE)
+
+/**
+ * Ensures `x` is not a POSIX error, otherwise the function will return
+ */
+#define GUARD_POSIX_VOID( x )                       __S2N_ENSURE((x) >= S2N_SUCCESS, return )
+
 
 /**
  * Ensures `x` is not a OpenSSL error, otherwise the function will `BAIL` with `error`
